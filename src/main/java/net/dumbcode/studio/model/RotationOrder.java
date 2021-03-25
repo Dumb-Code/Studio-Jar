6044498267cef55fe86d16023d10715154e0950f
Wyn Price <wynprice999@gmail.com>
2021-03-25 10:08:50 +0000

    Added more options to rotation order

--- src/main/java/net/dumbcode/studio/model/RotationOrder.java
+++ src/main/java/net/dumbcode/studio/model/RotationOrder.java
@@ -1,10 +1,32 @@
 package net.dumbcode.studio.model;
 
 public enum RotationOrder {
-    XYZ,
-    YXZ,
-    ZXY,
-    ZYX,
-    YZX,
-    XZY;
+    XYZ(0, 1, 2),
+    YXZ(1, 0, 2),
+    ZXY(2, 0, 1),
+    ZYX(2, 1, 0),
+    YZX(1, 2, 0),
+    XZY(0, 2, 1);
+
+    private final int first;
+    private final int second;
+    private final int third;
+
+    RotationOrder(int first, int second, int third) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+
+    public int getFirst() {
+        return first;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public int getThird() {
+        return third;
+    }
 }
