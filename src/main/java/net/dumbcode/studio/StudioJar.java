f8107eeb08e9856a749433ced910920bc250e66e
Wyn Price <wynprice999@gmail.com>
2021-03-26 01:43:11 +0000

    Oi oi stop spyin on me file system

--- src/main/java/net/dumbcode/studio/StudioJar.java
+++ src/main/java/net/dumbcode/studio/StudioJar.java
@@ -1,18 +1,9 @@
 package net.dumbcode.studio;
 
-import net.dumbcode.studio.model.*;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class StudioJar {
 
 	public static void main(String[] args) throws IOException {
-		File file = new File("C:\\Users\\wynpr\\Downloads\\New Model (3) (2) (1).dcm");
-		ModelInfo info = ModelLoader.loadModel(new FileInputStream(file), RotationOrder.ZYX, ModelMirror.XY);
-		ModelWriter.writeModel(info, new FileOutputStream("C:\\Users\\wynpr\\Downloads\\New Model_out.dcm"));
-
 	}
 }
